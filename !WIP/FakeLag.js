e5f8b788a09830768bde98e30578d9a415b1b8a3
Kash <149321663+Cata50@users.noreply.github.com>
2024-01-03 12:03:31 -0500

    blinkless fakelag, new exhi

--- !WIP/FakeLag.js
+++ !WIP/FakeLag.js
@@ -1,6 +1,5 @@
 // MODULE
 var module = rise.registerModule("FakeLag", "Makes you look like lagging serversidedly - By Cata50");
-module.registerSetting("mode", "Skip", "fall", ["fall", "jump", "both"]);
 
 // FUNCTION onTick
 module.handle("onTick", function () {
@@ -8,26 +7,12 @@ module.handle("onTick", function () {
     var speed = rise.getModule("Speed");
     var ground = player.isOnGround();
     var fall = player.getFallDistance();
-    var mode = module.getSetting("Skip");
 
-    if (mode === "fall") {
-        if (speed && fall > 0 && fall <= 1.145) {
-            rise.blink();
-        } else {
-            rise.dispatch();
-        }
-    } else if (mode === "jump") {
-        if (speed && !ground && fall <= 0) {
-            rise.blink();
-        } else {
-            rise.dispatch();
-        }
-    } else if (mode === "both") {
-        if (!ground && fall <= 1.145) {
-            rise.blink();
-        } else {
-            rise.dispatch();
-        }
+
+    if (speed.isEnabled() && fall > 0 && fall <= 1.145) {
+        rise.blink();
+    } else {
+        rise.dispatch();
     }
 });
 
