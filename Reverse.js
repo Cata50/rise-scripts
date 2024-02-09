0cc94028ac2feaf038ce2d25602a6e96df61a0d5
Kash <149321663+Cata50@users.noreply.github.com>
2024-02-08 20:42:40 -0500

    ㅤ

--- Reverse.js
+++ Reverse.js
@@ -2,16 +2,19 @@
 var module = rise.registerModule("Reverse", "Hypixel Damageboost");
 module.registerSetting("mode", "ScriptMode", "Velo", "Velo");
 module.setSettingVisibility("ScriptMode", false);
+module.registerSetting("boolean", "Hold Jump", false);
 
 // FUNCTION ONTICK
 module.handle("onTick", function () {
+    var holdJump = module.getSetting("Hold Jump");
+    var jumping = input.isKeyBindJumpDown();
     var hurt = player.getHurtTime();
     var motion = player.getMotion();
     var speed = rise.getModule("Speed");
     var velo = rise.getModule("Velocity");
     var scaffold = rise.getModule("Scaffold");
 
-    if (speed.isEnabled()) {
+    if (speed.isEnabled() && (holdJump ? jumping : !player.isDead())) {
         velo.setEnabled(false);
         if (hurt === 9) {
             player.setMotionX(-motion.x);
